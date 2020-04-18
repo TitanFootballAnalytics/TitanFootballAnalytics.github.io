c773d6e066c3b5ccf8941d81a0b2dbe46c1a0381
nb486 <nb486@cornell.edu>
2020-04-18 15:52:11 -0400

    Added DriveChart and finished sankey

--- graphingPackage.js
+++ graphingPackage.js
@@ -153,14 +153,18 @@ function addBarGraph(data, uniqueID ,canvas, x1, y1, x2, y2){
 
 
 function addSankey(data, canvas, x1, y1, x2, y2){
-    const margin2 = {top:0,bottom:0,left:0,right:0};
-    var max = d3.max(data.map(d => d.val));
-    var min = d3.min(data.map(d => d.val));
-
-    y = d3.scaleBand().rangeRound([y1, y2]);
-    x = d3.scaleLinear().rangeRound([x1, x2]);
-    y.domain(data);
-    x.domain([0,1,2]);
+    x1 = 100;
+    y1 = 100;
+    x2 = 400;
+    y2 = 400;
+    // const margin2 = {top:0,bottom:0,left:0,right:0};
+    // var max = d3.max(data.map(d => d.val));
+    // var min = d3.min(data.map(d => d.val));
+    //
+    // y = d3.scaleBand().rangeRound([y1, y2]);
+    // x = d3.scaleLinear().rangeRound([x1, x2]);
+    // y.domain(data);
+    // x.domain([0,1,2]);
 
     //x.domain([0, x2-x1]);
 
@@ -183,20 +187,230 @@ function addSankey(data, canvas, x1, y1, x2, y2){
     function toString(x,y){
       return "" + x + " " + y + "";
     }
+    //create start locations for bezier rect
+    var colors = [{r:238,g:32,b:77},
+                  {r:252,g:232,b:131},
+                  {r:31,g:117,b:254},
+                  {r:255,g:117,b:56},
+                  {r:28,g:172,b:120},
+                  {r:146,g:110,b:174}];
+    var color_start = Math.round(Math.random()*5);
+    console.log(color_start)
+    var total = 0;
+    var val = data[0].in;
+    for(var i = 0; i < data.length;i++){
+      if(data[i].in != val){
+        val = data[i].in;
+        color_start ++;
+        color_start = color_start%6;
+      }
+      data[i].color = colors[color_start];
+      data[i].start = total;
+      data[i].id = i;
+      total += data[i].count;
+    }
+
+
+
+    //sort in order to be able to easily set end locations
+    //sort will be on the out collumn first, since the in collumn is already ordered
+    //it is exepcted that the it will remain ordered after a bubble sort, within
+    //each bin
+
+    for(var i = 0; i < data.length - 1; i++){
+      for(var j = 0; j < data.length-1; j++){
+        if(data[j].out > data[j+1].out){
+          //swap!
+          var temp = data[j];
+          data[j] = data[j+1];
+          data[j+1] = temp;
+        }
+      }
+    }
+
+
+
+
+
+
+    //end order calculation
+    var sum = 0;
+    for(var i = 0; i < data.length;i++){
+      data[i].end = sum;
+      sum += data[i].count;
+    }
+
+
+
+
+
+    var vert_scale = (y2-y1)/total;
+
+    x2 += 40;
+    var gap = 10;
+    var colors = [{r:238,g:32,b:77},
+                  {r:252,g:232,b:131},
+                  {r:31,g:117,b:254},
+                  {r:255,g:117,b:56},
+                  {r:28,g:172,b:120},
+                  {r:146,g:110,b:174}];
+    var color_start = Math.round(Math.random()*5);
+    //=========================================================================
+    var first = data[0].out;
+    var start = data[0].end;
+    var end = data[0].count+start;
+    var color = colors[color_start];
+    data[0].end_color = color;
+    for(var i = 1; i < data.length;i++){
+
+
+        console.log(i)
+        console.log(first);
+        console.log(data[i].out);
+        console.log("----------");
+      if(data[i].out != first){
+          console.log("gottem")
+
+          canvas.append("path")
+          .attr("d"," M " + toString(x2+gap,y1+start*vert_scale)+
+                    " H " + (x2+gap+30) +
+                    " V " + (y1+end*vert_scale)+
+                    " H " + (x2+gap))
+
+          .attr("fill","rgb("+color.r+"," + color.g+"," + color.b+")");
+          color_start = (color_start+1)%colors.length;
+
+
+          first = data[i].out;
+          start = data[i].end;
+          end = data[i].count +start;
+          color = colors[color_start];
+          data[i].end_color = color;
+      }
+
+      else{
+        data[i].end_color = color;
+        end = data[i].end+data[i].count;
+      }
+    }
+    end = data[data.length-1].end + data[data.length-1].count;
+    data[data.length-1].end_color = color;
     canvas.append("path")
-    .attr("stroke","black")
-    .attr("stroke-width","1")
-    .attr("d","M "+toString(x1,y1)+
-              " L "+toString(x2,y1)+
-              " L "+toString(x2,y2)+
-              " L "+toString(x1,y2)+
-              " L "+toString(x1,y1))
-    .attr("fill","none");
+    .attr("d"," M " + toString(x2+gap,y1+start*vert_scale)+
+              " H " + (x2+gap+30) +
+              " V " + (y1+end*vert_scale)+
+              " H " + (x2+gap))
+    .attr("fill","rgb("+color.r+"," + color.g+"," + color.b+")");
+    //=========================================================================
+
+    //reorder for aesthetic
+    for(var i = 0; i < data.length - 1; i++){
+      for(var j = 0; j < data.length-1; j++){
+        if(data[j].id > data[j+1].id){
+          //swap!
+          var temp = data[j];
+          data[j] = data[j+1];
+          data[j+1] = temp;
+        }
+      }
+    }
+
+    console.log(data);
+
+
+    console.log(data)
+    // canvas.append("path")
+    // .attr("stroke","black")
+    // .attr("stroke-width","1")
+    // .attr("d"," M "+toString(x1,y1)+
+    //           " L "+toString(x2,y1)+
+    //           " L "+toString(x2,y2)+
+    //           " L "+toString(x1,y2)+
+    //           " L "+toString(x1,y1))
+    // .attr("fill","white");
+
+
+    var first = data[0].in;
+    var start = data[0].start;
+    var end = data[0].count+start;
+    var color = data[0].color;
+    console.log(data)
+    console.log(color)
+    for(var i = 1; i < data.length;i++){
+        // console.log(first);
+        // console.log(data[i].in);
+        // console.log("----------");
+      if(data[i].in != first){
+          console.log(color)
+          canvas.append("path")
+          .attr("d"," M " + toString(x1,y1+start*vert_scale)+
+                    " H " + (x1+30) +
+                    " V " + (y1+end*vert_scale)+
+                    " H " + x1)
+          .attr("fill","rgb("+color.r+"," + color.g+"," + color.b+")");
+          first = data[i].in;
+          start = data[i].start;
+          end = data[i].count +start;
+          color = data[i].color;
+      }
+      else if(i == data.length-1){
+        end = data[i].start + data[i].count;
+        canvas.append("path")
+        .attr("d"," M " + toString(x1,y1+start*vert_scale)+
+                  " H " + (x1+30) +
+                  " V " + (y1+end*vert_scale)+
+                  " H " + x1)
+        .attr("fill","rgb("+color.r+"," + color.g+"," + color.b+")");
+      }
+      else{
+        end = data[i].start+data[i].count;
+      }
+    }
+    x1 += 40;
+    var pad = 2;
     canvas.selectAll(".line")
         .data(data)
         .enter().append("path")
         .attr("class", "line")
-        .attr("d","M "+toString(x1,y1))
+        // .attr("stroke","black")
+        // .attr("stroke-width","1")
+        .attr("d",function(d,i){
+          // console.log(" M "+toString(x1,y1+d.start*vert_scale)+
+          //            " L "+toString(x2,y1+d.end*vert_scale) );
+          //
+          //parallelogram version:
+        // return    (" M "+toString(x1,y1+d.start*vert_scale)+
+        //            " L "+toString(x2,y1+d.end*vert_scale)+
+        //            " L "+toString(x2,y1+d.end*vert_scale + d.count*vert_scale)+
+        //            " L "+toString(x1,y1+d.start*vert_scale + d.count*vert_scale)+
+        //            " Z ");
+        var mid = (x2-x1)/2;
+       return    (" M "+toString(x1,y1+d.start*vert_scale+pad)+
+                  " C "+toString(x1+mid,y1+d.start*vert_scale+pad)+" "+toString(x1+mid,y1+d.end*vert_scale+pad)+" "+toString(x2,y1+d.end*vert_scale+pad)+
+                  " L "+toString(x2,y1+d.end*vert_scale + d.count*vert_scale-pad)+
+                  " C "+toString(x1+mid,y1+d.end*vert_scale + d.count*vert_scale-pad)+" "+toString(x1+mid,y1+d.start*vert_scale + d.count*vert_scale-pad)+" "+toString(x1,y1+d.start*vert_scale + d.count*vert_scale-pad)+
+                  " Z ");
+
+        })
+        .attr("fill",function (d, i) {
+            var col = d.color;
+            var ecol = d.end_color;
+            // <defs>
+              // <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+              //   <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+              //   <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+              // </linearGradient>
+            // </defs>
+            canvas.insert("defs").html("<linearGradient id='grad"+i+"' x1='0%' y1='0%' x2='100%' y2='0%'>"+
+                                          "<stop offset='0%' style='stop-color:rgb("+col.r+"," + col.g + "," + col.b  + ");stop-opacity:.5' />"+
+                                          "<stop offset='100%' style='stop-color:rgb("+col.r+"," + col.g + "," + col.b  + ");stop-opacity:.5' />"+
+                                        "</linearGradient>");
+            return "url(#grad"+i+")"//'rgba('+col.r+', ' + col.g + ', ' + col.b  + ',.5)'
+        	})
+        // .attr("fill",function (d, i) {
+        //     var col = d.color;
+        // 		return 'rgba('+col.r+', ' + col.g + ', ' + col.b  + ',.5)'
+        // 	})
 //         .attr("transform","translate("+(margin2.left+x1)+","+(margin2.top+y1)+")")
 //         .attr("class", "bar")
 //         .attr("fill", function (d, i) {
@@ -217,6 +431,98 @@ function addSankey(data, canvas, x1, y1, x2, y2){
 
 }
 
+
+function addFieldChart(data,canvas,x1,y1,x2,y2){
+  let p1 = {x:100.0,y:400.0};
+  let p2 = {x:580.0,y:400.0};
+  let pm = {x:340.0,y:-80.0};
+  let p = .25;
+  function toString(x,y){
+    return "" + x + " " + y + "";
+  }
+
+  canvas.append("path")
+  .attr("d"," M " + toString(p1.x,p1.y)+
+            " L " + toString(pm.x,pm.y) +
+            " L " + toString(p2.x,p2.y)+
+            " Z ")
+  .attr("fill","rgba(255,255,255,0)");
+
+  function newPoint(iP,fP,p){
+
+    var x = (1.0-p)*iP.x + p*fP.x;
+    var y = (1.0-p)*iP.y + p*fP.y;
+    return {x:x,y:y};
+  }
+
+  console.log("d"," M " + toString(p1.x,p1.y)+
+            " L " + toString(newPoint(p1,pm,p).x,newPoint(p1,pm,p).y) +
+            " L " + toString(newPoint(p2,pm,p).x,newPoint(p2,pm,p).y)+
+            " L " + toString(p2.x,p2.y)+
+            " Z ")
+  canvas.append("path")
+  .attr("d"," M " + toString(p1.x,p1.y)+
+            " L " + toString(newPoint(p1,pm,p).x,newPoint(p1,pm,p).y) +
+            " L " + toString(newPoint(p2,pm,p).x,newPoint(p2,pm,p).y)+
+            " L " + toString(p2.x,p2.y)+
+            " Z ")
+  .attr("fill","white");
+
+  var pad = 2;
+  var step = (p2.x-p1.x)/12;
+
+  for(var i = 1; i <= 12; i++){
+    var color = "green"
+    if(i == 1 || i == 12 ){
+      color = "grey";
+    }
+    var tp1 = {x:p1.x+(i-1)*step,y:p1.y};
+    var tp2 = {x:p1.x+(i)*step,y:p1.y};
+    canvas.append("path")
+    .attr("d"," M " + toString(tp1.x+pad,tp1.y-2*pad)+ //TODO: add padding within function calls, not to output
+              " L " + toString(newPoint(tp1,pm,p).x+pad,newPoint(tp1,pm,p).y+2*pad) +
+              " L " + toString(newPoint(tp2,pm,p).x-pad,newPoint(tp2,pm,p).y+2*pad)+
+              " L " + toString(tp2.x-pad,tp2.y-2*pad)+
+              " Z ")
+    .attr("fill",color);
+  }
+  var count = 6;
+  var base = newPoint(p1,pm,p).y + 2*pad;
+  var bottom = p1.y - 2*pad;
+  var stepsize = (bottom-base)/count;
+
+  // [{start:10,end:15,type:"pass"},
+  // {start:15,end:14,type:"run"},
+  // {start:14,end:22,type:"run"},
+  // {start:22,end:34,type:"pass"},
+  // {start:34,end:40,type:"pass"}]
+  var zeroYDS = newPoint(p1,p2,1/12);
+  var frankyYDS = newPoint(p1,p2,11/12);
+  for(var i = 0; i < count;i++){
+    var color = "blue";
+    if(data[i].type=="run"){
+      color = "red";
+    }
+    var b1 = newPoint(zeroYDS,frankyYDS,data[i].start/100);
+    var b2 = newPoint(zeroYDS,frankyYDS,data[i].end/100);
+    var tp1 = newPoint(b1,pm,(p-p*((i+1)/count)));
+    var tp2 = newPoint(b1,pm,(p-p*((i)/count)));
+    var tp3 = newPoint(b2,pm,(p-p*((i)/count)));
+    var tp4 = newPoint(b2,pm,(p-p*((i+1)/count)));
+    canvas.append("path")
+    .attr("d"," M " + toString(tp1.x,tp1.y-pad)+
+              " L " + toString(tp2.x,tp2.y+pad)+
+              " L " + toString(tp3.x,tp3.y+pad)+
+              " L " + toString(tp4.x,tp4.y-pad)+
+              " Z ")
+    .attr("fill",color);
+  }
+
+
+  //create white field background
+
+
+}
 
 
 function addHeader(svg) {
