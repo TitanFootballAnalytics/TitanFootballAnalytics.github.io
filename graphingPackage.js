3e641b032e669a4c7c8021d56a72a32b534e0602
Kevin <kah279@cornell.edu>
2020-04-26 17:10:04 -0400

    working on bar graphs

--- graphingPackage.js
+++ graphingPackage.js
@@ -120,16 +120,16 @@ function addBarGraph(data, uniqueID, canvas, x1, y1, x2, y2) {
   x.domain([min - (max) / 10, max]);
 
   //add the y axis to the graph
-  canvas.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + (margin2.left + x1) + "," + (margin2.top + y1) + ")")
-    .call(d3.axisLeft(y).ticks(6))
+  // canvas.append("g")
+  //   .attr("class", "y axis")
+  //   .attr("transform", "translate(" + (margin2.left + x1) + "," + (margin2.top + y1) + ")")
+  //   .call(d3.axisLeft(y).ticks(data.length))
 
   //add the x axisto the graph
   canvas.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(" + (margin2.left + x1) + "," + (y2 + margin2.top + 10) + ")")
-    .call(d3.axisBottom(x).ticks(4));
+    .call(d3.axisBottom(x).ticks(2));
 
   canvas.selectAll(".bar" + uniqueID)
     .data(data)
@@ -172,6 +172,15 @@ function addBarGraph(data, uniqueID, canvas, x1, y1, x2, y2) {
     .attr("cx", d => -10)
     .attr("cy", d => y(d.category) + y.bandwidth() / 2)
     .attr("r", 5)
+
+    for(let i = 0; i < data.length; i++){
+        canvas.append("text")
+          .text(data[i].category)
+          .attr("fill","black")
+          .attr("stroke","black")
+          .attr("stroke-width",1)
+          .attr("transform","translate("+x1+","+(y1+y(data[i].category)+(y.bandwidth() / 2))+") rotate(10)")
+    }
 };
 
 
