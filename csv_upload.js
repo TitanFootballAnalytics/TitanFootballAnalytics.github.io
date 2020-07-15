6484c7330a880f2bf9e0e27b3d711d14ce0e74fb
nb486 <nb486@cornell.edu>
2020-07-15 11:26:35 -0400

    finalized first draft of upload page

--- csv_upload.js
+++ csv_upload.js
@@ -1,6 +1,6 @@
 var svg;
 const standardset = ["Down","Distance","FieldZone","Personnel","Formation","PlayType","FormationFam","Cover","CoverFam"];
-
+document.getElementById("submitButton").addEventListener("click",submitHandler,false);
 createRecepticle(standardset);
 // d3.select("#mainDiv").append("svg")
 // .attr("width", 2000)
@@ -95,11 +95,13 @@ function allowDrop(ev) {
 function drag(ev) {
 	// ev.target.children[0].style.visibility = "hidden"
   ev.dataTransfer.setData("text", ev.target.id);
-	console.log(ev.target.id)
+	// ev.target.style.cursor = 'move';
+	// console.log(ev.target.id)
 	// console.log("dataget");
 }
 
 function drop(ev) {
+
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
 	var sourceBoxD3 = d3.select("#"+data).style("background-color","red");
@@ -108,11 +110,12 @@ function drop(ev) {
 	sourceBoxD3.attr("draggable","false");
 	// console.log(sourceBox)
 	var targetBoxD3 = d3.select(ev.target);
-
+	ev.target.removeEventListener('dragover', allowDrop);
+	ev.target.removeEventListener('drop', drop);
 	targetBoxD3.style("z-index","3")
 					 .style("background-color","green");
 	fitText(ev.target,sourceBoxD3._groups[0][0].labelTitan)
-	sourceBox.addEventListener('click', function() {undoSelect(targetBoxD3,sourceBoxD3,sourceBox)}, false);
+	sourceBox.addEventListener('click', function() {undoSelect(ev.target,targetBoxD3,sourceBoxD3,sourceBox)}, false);
   // ev.target.appendChild(document.getElementById(data));
 }
 
@@ -122,8 +125,10 @@ function drop(ev) {
 
 
 
-function undoSelect(targetD3,sourceD3,source){
+function undoSelect(target,targetD3,sourceD3,source){
 	 targetD3.node().removeChild(targetD3.node().firstChild);
+	 target.addEventListener('dragover', allowDrop, false);
+	 target.addEventListener('drop', drop, false);
 	 //var old_element = document.getElementById("btn");
 	 sourceD3.style("background-color","white");
 	 targetD3.style("background-color","white")
@@ -135,6 +140,10 @@ function undoSelect(targetD3,sourceD3,source){
 	 new_element.labelTitan = source.labelTitan;
 
 	 new_element.addEventListener('dragstart', drag, false);
+}
+
+function submitHandler(){
+
 }
 
 function generateHoldingCell(data){
@@ -212,6 +221,7 @@ $(document).ready(function() {
 			this.data = data;
 			generateHoldingCell(data[0]);
 			StatsProcessor(data);
+
 			$("#statOutPut").removeClass( "hidden" );
 			$("#errorOutPut").addClass( "hidden" );
 		} else {
@@ -222,295 +232,5 @@ $(document).ready(function() {
 	};
 	document.getElementById('txtFileUpload').addEventListener('change', uploadFile, false);
 
-    // var clicked = [];
-    // var no_lines = [];
-		//
-    // function glow(shape) {
-    //     var new_opacity, new_width;
-    //     new_opacity = 1;
-    //     new_width = 3;
-    //     shape.transition().duration(200)
-    //         .attr("fill-opacity", new_opacity)
-    //         .attr("stroke-width", new_width);
-    // }
 
-    // function swap(s1, s2) {
-    //     var id1 = s1.attr("id");
-    //     var ind1 = s1.attr("index");
-    //     var y1 = s1.attr("y");
-    //     console.log("swapping " + id1 + "(" + y1 + ")" + " with " + s2.attr("id") + "(" + s2.attr("y") + ")");
-		//
-    //     s1.transition().duration(1000)
-    //         .attr("id", s2.attr("id"))
-    //         .attr("index", s2.attr("index"))
-    //         .attr("y", s2.attr("y"))
-    //     s2.transition().duration(1000)
-    //         .attr("id", id1)
-    //         .attr("index", ind1)
-    //         .attr("y", y1);
-    // }
-		//
-    // function swap_text(s1, s2) {
-    //     var id1 = s1.attr("id");
-    //     var y1 = s1.attr("y");
-		//
-    //     s1.transition().duration(1000)
-    //         .attr("id", s2.attr("id"))
-    //         .attr("y", s2.attr("y"));
-    //     s2.transition().duration(1000)
-    //         .attr("id", id1)
-    //         .attr("y", y1);
-    // }
-
-    // function mouseon(shape, index) {
-    //   shape.on("mouseover", function () {
-    //     var s = d3.select(this);
-    //     var new_op, new_st;
-    //     // console.log(parseInt(s.attr("selected"),10));
-    //     if (parseInt(s.attr("selected"),10) == 1) {
-    //       new_op = 1;
-    //       new_st = 4;
-    //     }
-    //     else {
-    //       new_op = 0.8;
-    //       new_st = 2;
-    //     }
-    //     shape.transition().duration(200)
-    //       .attr("fill-opacity", new_op)
-    //       .attr("stroke-width", new_st)
-    //   });
-    // }
-		//
-    // function mouseoff(shape, index) {
-    //   shape.on("mouseout", function () {
-    //     console.log("off");
-    //     var s = d3.select(this);
-    //     var new_op, new_st;
-    //     // console.log(parseInt(s.attr("selected"), 10));
-    //     if (parseInt(s.attr("selected"),10) == 1 || parseInt(s.attr("connected"),10) == 1) {
-    //       new_op = 1;
-    //       new_st = 3;
-    //     }
-    //     else {
-    //       new_op = 0.6;
-    //       new_st = 1;
-    //     }
-    //     shape.transition().duration(200)
-    //       .attr("fill-opacity", new_op)
-    //       .attr("stroke-width", new_st)
-    //   });
-    // }
-		//
-    // function mouseclick(shape) {
-    //     var new_opacity, new_width;
-    //     if (parseInt(shape.attr("selected"),10) == 0) {
-    //         new_opacity = 1;
-    //         new_width = 3;
-    //     }
-    //     else {
-    //         new_opacity = 0.6;
-    //         new_width = 1;
-    //     }
-    //     shape.transition().duration(200)
-    //         .attr("fill-opacity", new_opacity)
-    //         .attr("stroke-width", new_width)
-    //         .attr("selected", Math.pow(parseInt(shape.attr("selected"),10) - 1, 2));
-    //     // console.log(shape.attr("selected"));
-    // }
-
-    // function disconnect(index) {
-    //     var res = d3.select("#result" + index);
-    //     var col = d3.select("#column" + index);
-    //     var lin = d3.select("#line" + index);
-		//
-    //     res.attr("connected", 0);
-    //     col.attr("connected", 0);
-    //     lin.attr("visibility", "hidden");
-		//
-    //     function deselect(shape) {
-    //         var new_opacity, new_width;
-    //         new_opacity = 0.6;
-    //         new_width = 1;
-    //         shape.transition().duration(200)
-    //             .attr("fill-opacity", new_opacity)
-    //             .attr("stroke-width", new_width)
-    //             .attr("selected", 0);
-    //     }
-    //     deselect(res);
-    //     deselect(col);
-    // }
-		//
-    // function click(shape, index) {
-    //     shape.on("mousedown", function () {
-    //         // shape.attr("connected", 1);
-    //         var id = shape.attr("id")
-    //         var chosen = d3.select("#" + clicked[0]);
-    //         // console.log(id);
-		//
-    //         if (shape.attr("connected") == 1) {
-    //             console.log("disconnecting");
-    //             disconnect(shape.attr("index"));
-    //             // clicked = [];
-    //         }
-    //         else if (clicked.length == 0) {
-    //             console.log("push");
-    //             mouseclick(shape);
-    //             clicked.push(id);
-    //         }
-    //         // If "shape" is connected to others
-    //         else if (shape.attr("id") == chosen.attr("id")) {
-    //             console.log("duplicate");
-    //             mouseclick(shape);
-    //             clicked = [];
-    //         }
-    //         else if (chosen.attr("class") == shape.attr("class") && chosen.attr("selected") == 1) {
-    //             console.log("swap same col");
-    //             mouseclick(chosen);
-    //             mouseclick(shape);
-    //             clicked = [shape.attr("id")];
-    //         }
-    //         else if (chosen.attr("class") != shape.attr("class")) {
-    //             console.log("choosing");
-    //             var dest_ind;
-    //             var lin;
-    //             // glow(chosen);
-    //             // glow(shape);
-    //             chosen.attr("connected", 1);
-    //             chosen.attr("selected", 0);
-    //             shape.attr("connected", 1);
-    //             shape.attr("selected", 0);
-		//
-    //             if (chosen.attr("class") == "column") {
-		//
-    //                 dest_ind = shape.attr("index")
-    //                 if (chosen.attr("index") != dest_ind) {
-    //                     swap(chosen, d3.select("#column" + dest_ind));
-    //                     swap_text(d3.select("#text" + chosen.attr("index")), d3.select("#text" + dest_ind));
-    //                 }
-    //                 lin = d3.select("#line" + dest_ind);
-    //                 lin.attr("visibility", "visible");
-    //             }
-    //             else if (chosen.attr("class") == "result") {
-    //                 console.log("bottom");
-		//
-    //                 dest_ind = chosen.attr("index");
-    //                 if (shape.attr("index") != dest_ind) {
-    //                     swap(shape, d3.select("#column" + dest_ind));
-    //                     // glow(shape);
-    //                     // shape.attr("selected", 0);
-    //                     swap_text(d3.select("#text" + shape.attr("index")), d3.select("#text" + dest_ind));
-    //                 }
-    //                 lin = d3.select("#line" + dest_ind);
-    //                 lin.attr("visibility", "visible");
-    //             }
-    //             clicked = [];
-    //         }
-    //         else {
-    //             console.log("ERROR");
-    //         }
-    //     }
-    // )}
-
-    // function drawConnections(canvas, lenResults, top, left, height, width, separation, distance){
-    //     var lines = [];
-    //     for (var i = 0 ; i < lenResults ; i++){
-    //         lines.push(canvas.append("path")
-    //             .attr("d", "M " + (left + width) + " " + (top + height/2 + separation*i) + " L " + (left + distance) + " " + (top + height/2 + separation*i))
-    //             .attr("stroke-width", 2)
-    //             .attr("fill", "black")
-    //             .attr("fill-opacity", 0.6)
-    //             .attr("stroke", "red")
-    //             .attr("visibility", "hidden")
-    //             .attr("id", "line" + i)
-    //             .attr("index", i));
-    //         }
-    //     return lines;
-    // }
-
-    // function drawColumns(canvas, columnNames, topMargin, leftMargin, blockHeight, blockWidth, blockSeparationY) {
-    //     var lines = drawConnections(canvas, 3, topMargin, leftMargin, blockHeight, blockWidth, blockSeparationY, 500);
-    //     var temp = topMargin;
-    //     var count = columnNames.length;
-    //     var lines = drawConnections(canvas, count, 3, topMargin, leftMargin, blockHeight, blockWidth, blockSeparationY, 500);
-    //     var columns = [];
-    //     var column_text = [];
-    //     var results = [];
-    //     var result_text = [];
-		//
-    //     for (var i = 0; i < count ; i++){
-    //         columns.push(canvas.append("rect")
-    //             .attr("x", leftMargin)
-    //             .attr("y", topMargin)
-    //             .attr("height", blockHeight)
-    //             .attr("width", blockWidth)
-    //             .attr("fill", "orange")
-    //             .attr("fill-opacity", 0.6)
-    //             .attr("stroke-width", 1)
-    //             .attr("stroke", "maroon")
-    //             .attr("id", "column" + i)
-    //             .attr("class", "column")
-    //             .attr("index", i)
-    //             .attr("selected", 0)
-    //             .attr("connected", 0))
-    //         column_text.push(canvas.append("text")
-    //             .attr("x", leftMargin + blockWidth/2)
-    //             .attr("y", topMargin + blockHeight/2 + 16/4)
-    //             .attr("fill", "#00203FFF")
-    //             .attr("stroke", "#00203FFF")
-    //             .text(columnNames[i])
-    //             .attr("text-anchor", "middle")
-    //             .attr("font-size", 16 + "px")
-    //             .attr("width", blockWidth/2)
-    //             .attr("height", blockHeight/2)
-    //             .attr("id", "text" + i))
-    //         topMargin += blockSeparationY;
-    //     }
-    //     topMargin = temp;
-    //     for (var j = 0; j < standardset.length; j++){
-    //         results.push(canvas.append("rect")
-    //             .attr("x", leftMargin + 500)
-    //             .attr("y", topMargin)
-    //             .attr("height", blockHeight)
-    //             .attr("width", blockWidth)
-    //             .attr("fill", "orange")
-    //             .attr("fill-opacity", 0.6)
-    //             .attr("stroke-width", 1)
-    //             .attr("stroke", "maroon")
-    //             .attr("id", "result" + j)
-    //             .attr("class", "result")
-    //             .attr("index", j)
-    //             .attr("selected", 0)
-    //             .attr("connected", 0))
-    //         result_text.push(canvas.append("text")
-    //             .attr("x", leftMargin + blockWidth/2 + 500)
-    //             .attr("y", topMargin + blockHeight/2 + 16/4)
-    //             .attr("fill", "#00203FFF")
-    //             .attr("stroke", "#00203FFF")
-    //             .text(standardset[j])
-    //             .attr("text-anchor", "middle")
-    //             .attr("font-size", 16 + "px")
-    //             .attr("width", blockWidth/2)
-    //             .attr("height", blockHeight/2))
-    //         topMargin += blockSeparationY;
-    //     }
-    //     columns.forEach(click);
-    //     results.forEach(click);
-    //     // columns.forEach(mouseclick);
-    //     // results.forEach(mouseclick);
-    //     columns.forEach(mouseoff);
-    //     results.forEach(mouseoff);
-    //     columns.forEach(mouseon);
-    //     results.forEach(mouseon);
-    //     // console.log(columns.forEach(x => x.attr("y")))
-    // }
-
-    // function changeNames(colNames, indices){
-    //     for (var i = 0; i < indices.length ; i++){
-    //         var before = parseInt(indices[i].slice(0,1), 10);
-    //         var after = parseInt(indices[i].slice(1,2), 10);
-    //         data[0][before] = "Data Point " + (after + 1);
-    //     }
-    //     // console.log(data);
-		//
-    // }
 });
