74c3952db98e221ca70467c0480c9b290700a8a4
nb486 <nb486@cornell.edu>
2020-08-06 03:24:32 -0400

    Remade lambda call integration, nearly finalized function set for auth.js s3 libraries

--- js/browsereport.js
+++ js/browsereport.js
@@ -62,82 +62,157 @@ authAndRun((team)=>{
     iteratedObjGet("titancommonstorage",keylst,[],(data)=>{
       data.forEach((d,i)=>{
         if(d.metadata){
-          console.log(d.metadata)
-          var card = mainholder.appendChild(document.createElement('div'));
-          card.className = "report-card-holder col-md-2";
-          var reportlst = card.appendChild(document.createElement('div'));
-          reportlst.className = "report-card"
-          var h5 = reportlst.appendChild(document.createElement('h5'));
-          h5.style.margin = "0px";
-          var h6 = reportlst.appendChild(document.createElement('h6'));
-          h6.style.marginTop = "0px";
-          var icon = h6.appendChild(document.createElement('i'));
-          icon.innerHTML = "Games Included";
-          var gamelst = reportlst.appendChild(document.createElement('ul'));
-          gamelst.className = "gameslist";
-          let boldtext = document.createElement('b');
-          let othertext = document.createElement('o');
-          boldtext.innerHTML = d.metadata["target_team"];
-          h5.appendChild(boldtext);
-          othertext.textContent  = " Scouting Report -"+d.metadata.reportid;
-          h5.appendChild(othertext);
+          if(d.metadata.reportid == getUrlParam("reportid","empty")){
+            console.log(d.metadata.reportid);
+            var card = mainholder.appendChild(document.createElement('div'));
+            card.className = "report-card-holder col-md-2";
+            var reportlst = card.appendChild(document.createElement('div'));
+            reportlst.className = "report-card"
+            var h5 = reportlst.appendChild(document.createElement('h5'));
+            h5.style.margin = "0px";
+            var h6 = reportlst.appendChild(document.createElement('h6'));
+            h6.style.marginTop = "0px";
+            var icon = h6.appendChild(document.createElement('i'));
+            icon.innerHTML = "Games Included";
+            var gamelst = reportlst.appendChild(document.createElement('ul'));
+            gamelst.className = "gameslist";
+            let boldtext = document.createElement('b');
+            let othertext = document.createElement('o');
+            boldtext.innerHTML = d.metadata["target_team"];
+            h5.appendChild(boldtext);
+            othertext.textContent  = " Scouting Report -"+d.metadata.reportid;
+            h5.appendChild(othertext);
 
-          for(var j = 0; j < d.metadata.files.length; j++){
-            var listitem = document.createElement("li");
-            listitem.innerHTML = d.metadata.files[j].split("/")[1];
-            gamelst.appendChild(listitem);
+            for(var j = 0; j < d.metadata.files.length; j++){
+              var listitem = document.createElement("li");
+              listitem.innerHTML = d.metadata.files[j].split("/")[1];
+              gamelst.appendChild(listitem);
+            }
+
+            var buttonDiv = document.createElement("div");
+            buttonDiv.id = "buttons";
+            buttonDiv.className = "buttonholder";
+            buttonDiv.align = "right";
+            reportlst.appendChild(buttonDiv);
+            var openButton = document.createElement("button");
+            openButton.innerHTML = "Open"
+            openButton.style.marginBottom = "5px";
+            openButton.type = "button";
+            openButton.className = "btn btn-primary";
+            openButton.addEventListener('click',(event)=>{openreport(d.metadata.reportid)},false);
+            buttonDiv.appendChild(openButton);
+
+            buttonDiv.appendChild(document.createElement("br"))
+
+            var editButton = document.createElement("button");
+            editButton.innerHTML = "Edit"
+            editButton.type = "button";
+            editButton.className = "btn btn-warning";
+            editButton.addEventListener('click',(event)=>{editreport(d.metadata.reportid)},false);
+            buttonDiv.appendChild(editButton);
+
+
+            reportlst.style.backgroundColor = "rgb(100,50,50)";
+            editButton.disabled = true;
+            openButton.disabled = true;
+
+            //when disabled:
+
+
+
+// BigRed/reports/HarvardDefense/data_BigRed_HarvardDefense.json
+            // console.log(d)//this is final mapping
+            // console.log("AWS REQUEST TYPE 1 HERE")
+            var reportid = d.metadata.reportid;
+            var keyCheck = `${team}/reports/${reportid}/data_${team}_${reportid}.json`;
+            var dir = `${team}/reports/${reportid}`;
+            // console.log(keyCheck)
+            listObjsAndRun(dir,"titancommonstorage",(data)=>{
+              // console.log(data.Contents);
+              if(data.Contents.some((obj)=>{return obj.Key === keyCheck})){
+                console.log("data detected");
+                //report already exists
+                reportlst.style.backgroundColor = "#1c1c1c";
+                editButton.disabled = false;
+                openButton.disabled = false;
+              }
+              else{
+                console.log("data not detected");
+                callLambdaAndRun(d,"generate_scorecards",(result)=>{
+                  console.log(result);
+                  if(result.FunctionError === "Unhandled"){
+                    reportlst.style.backgroundColor = "rgb(150,50,50)";
+                  }
+                  else{
+                    reportlst.style.backgroundColor = "#1c1c1c";
+                    editButton.disabled = false;
+                    openButton.disabled = false;
+                  }
+                });
+                // var result = JSON.parse(data.Payload)
+                // window.location.replace("scorecards.html?reportid="+message["metadata"]["reportid"]);
+              }
+            });
+            //awsrequest(finalmapping,reportid,1);
+
+
+
+
+          }
+          else{
+
+            var card = mainholder.appendChild(document.createElement('div'));
+            card.className = "report-card-holder col-md-2";
+            var reportlst = card.appendChild(document.createElement('div'));
+            reportlst.className = "report-card"
+            var h5 = reportlst.appendChild(document.createElement('h5'));
+            h5.style.margin = "0px";
+            var h6 = reportlst.appendChild(document.createElement('h6'));
+            h6.style.marginTop = "0px";
+            var icon = h6.appendChild(document.createElement('i'));
+            icon.innerHTML = "Games Included";
+            var gamelst = reportlst.appendChild(document.createElement('ul'));
+            gamelst.className = "gameslist";
+            let boldtext = document.createElement('b');
+            let othertext = document.createElement('o');
+            boldtext.innerHTML = d.metadata["target_team"];
+            h5.appendChild(boldtext);
+            othertext.textContent  = " Scouting Report -"+d.metadata.reportid;
+            h5.appendChild(othertext);
+
+            for(var j = 0; j < d.metadata.files.length; j++){
+              var listitem = document.createElement("li");
+              listitem.innerHTML = d.metadata.files[j].split("/")[1];
+              gamelst.appendChild(listitem);
+            }
+
+            var buttonDiv = document.createElement("div");
+            buttonDiv.id = "buttons";
+            buttonDiv.className = "buttonholder";
+            buttonDiv.align = "right";
+            reportlst.appendChild(buttonDiv);
+            var openButton = document.createElement("button");
+            openButton.innerHTML = "Open"
+            openButton.style.marginBottom = "5px";
+            openButton.type = "button";
+            openButton.className = "btn btn-primary";
+            openButton.addEventListener('click',(event)=>{openreport(d.metadata.reportid)},false);
+            buttonDiv.appendChild(openButton);
+
+            buttonDiv.appendChild(document.createElement("br"))
+
+            var editButton = document.createElement("button");
+            editButton.innerHTML = "Edit"
+            editButton.type = "button";
+            editButton.className = "btn btn-warning";
+            editButton.addEventListener('click',(event)=>{editreport(d.metadata.reportid)},false);
+            buttonDiv.appendChild(editButton);
           }
 
-          var buttonDiv = document.createElement("div");
-          buttonDiv.id = "buttons";
-          buttonDiv.className = "buttonholder";
-          buttonDiv.align = "right";
-          reportlst.appendChild(buttonDiv);
-          var openButton = document.createElement("button");
-          openButton.innerHTML = "Open"
-          openButton.style.marginBottom = "5px";
-          openButton.type = "button";
-          openButton.className = "btn btn-primary";
-          openButton.addEventListener('click',(event)=>{openreport(d.metadata.reportid)},false);
-          buttonDiv.appendChild(openButton);
-
-          buttonDiv.appendChild(document.createElement("br"))
-
-          var editButton = document.createElement("button");
-          editButton.innerHTML = "Edit"
-          editButton.type = "button";
-          editButton.className = "btn btn-warning";
-          editButton.addEventListener('click',(event)=>{editreport(d.metadata.reportid)},false);
-          buttonDiv.appendChild(editButton);
 
 
-
-          // <div class = "report-card-holder col-md-2" id="blank">
-          //     <div class="report-card" >
-          //       <h5 style="margin: 0px;"></h5>
-          //       <h6 style="margin-top: 0px;"><i>Games Included</i></h6>
-          //       <ul id="gameslist">
-          //       </ul>
-          //
-          //       <div id="buttons" class="buttonholder" align="right">
-                  // <button onclick="openreport(this.parentElement.parentElement.parentElement.id)"
-                  //         style="margin-bottom: 5px;"
-                  //         type="button"
-                  //         class="btn btn-primary">
-                  //         Open</button>
-                  // <br>
-                  // <button onclick="editreport(this.parentElement.parentElement.parentElement.id)"
-                  //         type="button"
-                  //         class="btn btn-warning">
-                  //         Edit</button>
-          //       </div>
-          //     </div>
         }
       });
     });
   });
 });
-
-
-
-// populatepage("1111")
