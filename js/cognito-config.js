3ecb342939b4d129a7e9d5439416e7f96b364055
Kunal Sheth <ks782@cornell.edu>
2020-08-21 21:29:00 -0400

    config

--- js/cognito-config.js
+++ js/cognito-config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_OIThgAgWl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3eaevu70267iv2mffmmcb38ul0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_cBl5ddpZ2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2d19vtmst82qctbnsj3lqt8cvn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
